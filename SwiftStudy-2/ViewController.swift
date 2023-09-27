1005691946df1e6f348c4dbcda9249325a3d5a11
KangRok Yoon <ykr0919@naver.com>
2023-09-28 02:22:18 +0900

    second commit

--- SwiftStudy-2/ViewController.swift
+++ SwiftStudy-2/ViewController.swift
@@ -7,17 +7,28 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UISearchResultsUpdating {
 
-    @IBOutlet weak var searchBar: UISearchBar!
+    @IBOutlet weak var myTable: UITableView!
+    
+    var candys = ["Chocolate Bar", "Chocolate Chip", "Dark Chocolate", "Lollipop", "Candy Cane", "Jaw Breaker", "Caramel", "Sour Chew", "Gummi Bear"]
+    
+    let searchController = UISearchController(searchResultsController: nil)
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        searchBar.layer.borderWidth = 0
-//        searchBar.layer.borderColor = UIColor.red.cgColor
+        title = "Search"
+        searchController.searchResultsUpdater = self
+        navigationItem.searchController = searchController
         
     }
-
+    
+    func updateSearchResults(for searchController: UISearchController) {
+        guard let text = searchController.searchBar.text else {
+            return
+        }
+        print(text)
+    }
+ 
 }
 
