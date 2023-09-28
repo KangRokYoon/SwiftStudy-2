45270b5e8a7fb7241d7dcd5501c1ff9081fb7bcc
KangRok Yoon <ykr0919@naver.com>
2023-09-28 15:27:26 +0900

    search 가능 구현 왼료

--- SwiftStudy-2/ViewController.swift
+++ SwiftStudy-2/ViewController.swift
@@ -7,28 +7,91 @@
 
 import UIKit
 
-class ViewController: UIViewController, UISearchResultsUpdating {
+class ViewController: UIViewController{
 
     @IBOutlet weak var myTable: UITableView!
     
     var candys = ["Chocolate Bar", "Chocolate Chip", "Dark Chocolate", "Lollipop", "Candy Cane", "Jaw Breaker", "Caramel", "Sour Chew", "Gummi Bear"]
     
+    var searching = false
+    var searchedCandys = [String]()
     let searchController = UISearchController(searchResultsController: nil)
     
     override func viewDidLoad() {
         super.viewDidLoad()
         title = "Search"
+        
+        searchController.loadViewIfNeeded()
         searchController.searchResultsUpdater = self
+        searchController.searchBar.delegate = self
+        searchController.obscuresBackgroundDuringPresentation = false
+        searchController.searchBar.enablesReturnKeyAutomatically = false
+        searchController.searchBar.returnKeyType = UIReturnKeyType.done
         navigationItem.searchController = searchController
+        navigationItem.hidesSearchBarWhenScrolling = false
+        definesPresentationContext = true
+        searchController.searchBar.placeholder = "Search"
         
     }
-    
-    func updateSearchResults(for searchController: UISearchController) {
-        guard let text = searchController.searchBar.text else {
-            return
-        }
-        print(text)
-    }
- 
 }
+  
+extension ViewController: UITableViewDelegate, UITableViewDataSource, UISearchBarDelegate, UISearchResultsUpdating{
+    func updateSearchResults(for searchController: UISearchController) {
+        let searchText = searchController.searchBar.text!
+        if !searchText.isEmpty
+        {
+            searching = true
+            searchedCandys.removeAll()
+            for candy in candys
+            {
+                if candy.lowercased().contains(searchText.lowercased())
+                {
+                    searchedCandys.append(candy)
+                }
+            }
+        }
+        else
+        {
+            searching = false
+            searchedCandys.removeAll()
+            searchedCandys = candys
+        }
+        myTable.reloadData()
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        if searching
+        {
+            return searchedCandys.count
+        }
+        else
+        {
+            return candys.count
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = myTable.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        if searching
+        {
+            cell.textLabel?.text = searchedCandys[indexPath.row]
+        }
+        else
+        {
+            cell.textLabel?.text = candys[indexPath.row]
+        }
+        return cell
+    }
+    func searchBarCancelButtonClicked(_ searchBar: UISearchBar) {
+        searching = false
+        searchedCandys.removeAll()
+        myTable.reloadData()
+    }
+         
+}
+    
+    
+
+ 
+
 
